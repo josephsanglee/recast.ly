677c62eda8dd3d98181a70988ae20fc7f8a3a4b4
josephsanglee <josephsanglee@gmail.com>
2016-09-30 20:06:28 -0700

    Create a reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var searchObj = {
+    part: 'snippet',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    videoEmbeddable: true,
+    type: 'video'
+  };
+
+  $.get('https://www.googleapis.com/youtube/v3/search', searchObj, 
+    function(data) {
+      callback(data.items);
+    });
 };
 
 window.searchYouTube = searchYouTube;
